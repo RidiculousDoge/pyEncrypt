e49853183e11c235d1897dd51b0955d754aa8ade
ridiculousDoge <313865185@qq.com>
2022-10-09 16:31:53 +0800

    upload pyEncrypt

---
+++ README.md
@@ -0,0 +1,80 @@
+# Simple Encrypt Tool
+## Main Goal
+- [x] locally config public key & private key
+- [x] used in command line, free to get & set password
+- [x] memorize in disk, output encrypted code
+
+## Data Structure
+- config written in json
+- data encrypted written in json file, working like:
+    ```json
+    {
+        "google":"213ab314cf",
+        "facebook":"12345fb3cd"
+    }
+    ```
+
+## Before using:
+
+### generate ssh
+```bash
+ssh-keygen -t rsa -m PEM
+```
+Then enter the directory you created `.ssh` file.
+```bash
+# in .ssh folder
+ssh-keygen -f id_rsa.pub -m PEM -e > id_rsa.pub.pem
+```
+
+will generate a `.ssh` folder in system path. In windows for example, the folder will be put default in `"C:\\Users\\%Username%\\.ssh"`
+
+The `-m PEM` argument is important in use, cause we use PEM style private & public key
+
+
+### Setup python environment
+```bash
+pip install rsa
+```
+
+### generate file
+```bash
+cd pyEncrypt
+echo {} >> output.json
+echo {} >> config.json
+```
+currently need this command cause the program will not automatically create an empty json file.
+## Usage
+### args:
+```bash
+usage: python encrypt.py [-h] [--set KEY VALUE] [--get GET] [--config ConfigName ConfigValue] [--all]
+```
+
+### key config
+```bash
+python encrypt.py --config ssh-folder <your ssh folder path>
+```
+
+### set password
+Then, to set a new password, use
+```bash
+python encrypt.py --set <your-account-name> <your-password>
+```
+e.g
+```bash
+python encrypt.py --set google 123456
+```
+
+### get password
+```bash
+python encrypt.py --get <your-account-name> [--all]
+```
+
+e.g
+```bash
+python encrypt.py --get google
+```
+
+## TODOs:
+- [ ] [ **feat** ] The program currently causes error when file not exist or not in json format. Automatically generate empty json file.
+- [ ] [ **feat** ] Release binary version to make it safer.
+- [ ] [ **doc** ] Update doc
---
+++ encrypt.py
@@ -0,0 +1,97 @@
+import rsa
+import json
+import argparse
+
+class client:
+
+    dict = {}
+    config = {}
+
+    configFile = "config.json"
+    outputFile = "output.json"
+    dataFile = outputFile
+
+    def readSavedPasswd(self):
+        # load to memory
+        print("----- loading saved passwd -----")
+        f = open(self.dataFile)
+        self.dict = json.load(f)
+        print("----- finished loading passwd -----")
+        # print(self.dict)
+            
+    def readKeyInConfig(self):
+        f = open(self.configFile)
+        self.config = json.load(f)
+        ssh_folder = self.config["ssh-folder"]
+        pubKeyPath = ssh_folder + "\\id_rsa.pub.pem"
+        priKeyPath = ssh_folder + "\\id_rsa"
+        self.privKey = rsa.PrivateKey.load_pkcs1(open(priKeyPath,"r").read())
+        self.pubKey = rsa.PublicKey.load_pkcs1(open(pubKeyPath,"r").read())
+        
+
+    def setNewPasswd(self,key,rawValue):
+        print("------- encrypting account %s, rawValue %s ------"%(key,rawValue))
+        encryptedPasswd = rsa.encrypt(rawValue.encode("utf-8"),self.pubKey)
+        self.dict[key] = encryptedPasswd.decode("latin1")
+        # print(encryptedPasswd)
+        print("------- encryption for account %s finished ------"%(key))
+
+    def getPasswdInMemory(self,key):
+        if key not in self.dict.keys():
+            print("-------not having this account of %s ------" %(key))
+            raise RuntimeError("NotHavingThisRecordError")
+        return rsa.decrypt(self.dict[key].encode("latin1"),self.privKey).decode("utf-8")
+        
+
+    def syncToOutputFile(self):
+        print("------ sync dict to disk ------")
+        with open(self.outputFile,"w",encoding="utf-8") as outfile:
+            json.dump(self.dict,outfile)
+        print("----- finished sync disk -----")
+
+    def syncToConfigFile(self):
+        print("----- syncing config file -----")
+        with open(self.configFile,"w",encoding="utf-8") as outfile:
+            json.dump(self.config,outfile)
+        print("----- finished sync config file -----")
+
+    def run(self,args):
+        try:
+            if(args.config):
+                # update config
+                self.config[args.config[0]] = args.config[1]
+                self.syncToConfigFile()
+
+            # read key
+            self.readKeyInConfig()
+            # read to memory
+            self.readSavedPasswd()
+
+            if(args.set):
+                # update new passwd
+                self.setNewPasswd(args.set[0],args.set[1])
+                self.syncToOutputFile()
+            if(args.get):
+                # get passwd
+                print(self.getPasswdInMemory(args.get))
+            if(args.all):
+                for key in self.dict:
+                    print("key for %s is %s"%(key,self.getPasswdInMemory(key)))
+        except RuntimeError as e:
+            print(e)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--set",type=str,nargs=2)
+    parser.add_argument("--get",type=str)
+    parser.add_argument("--config",type=str,nargs=2)
+    parser.add_argument("--all",action="store_true")
+    args = parser.parse_args()
+    
+
+    c = client()
+    c.run(args)
+
+    
+
+
